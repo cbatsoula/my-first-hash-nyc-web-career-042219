ebec87c5941746ab32714cac6ba326c32739b3d5
Catherine Batsoula <cbatsoula@gmail.com>
2019-04-13 00:44:10 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -2,9 +2,8 @@ def my_hash
   your_hash = {"nickname" => "Katya","hobby" => "knitting", "age" => 25 }
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 end
-
-
 def shipping_manifest
+  the_manifest = {"whale_bone_corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
